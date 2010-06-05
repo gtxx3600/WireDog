b11c56460bce04369db15e5bd9e123d07494ec61
sKabYY <skabyy@gmail.com>
2010-06-05 19:34:20 +0800

    Add search

--- lib/tool.py
+++ lib/tool.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python
 
-from parse import *
-
-
 def search_in_pkts(s,pkts):
     'searchstring(s,pkts) return a dict, keys are instance of parse.Pkt, value is a tuple (global_index,data_index,string startswith s)'
     ret = {}
     for pkt in pkts:
-        if pkt.__class__ is Pkt:
-            type = pkt.dict['order'][-1]
-            if type == 'TCP' or type == 'UDP':
-                d = pkt.dict[type]
-                index = d['data'].find(s)
-                if index >= 0 :
-                    global_index = index + 14 + pkt.dict['ip']['header_len'] + pkt.dict[type]['header_len'] 
-                    ret[pkt] = (global_index,index,d['data'][index:])
-                
-    return ret
-
-def search_in_pkt(s,pkt):
-    if pkt.__class__ is Pkt:
         type = pkt.dict['order'][-1]
         if type == 'TCP' or type == 'UDP':
             d = pkt.dict[type]
             index = d['data'].find(s)
             if index >= 0 :
                 global_index = index + 14 + pkt.dict['ip']['header_len'] + pkt.dict[type]['header_len'] 
-                return (global_index,index,d['data'][index:])
+                ret[pkt] = (global_index,index,d['data'][index:])
+                
+    return ret
+
+def search_in_pkt(s,pkt):
+    type = pkt.dict['order'][-1]
+    if type == 'TCP' or type == 'UDP':
+        d = pkt.dict[type]
+        index = d['data'].find(s)
+        if index >= 0 :
+            global_index = index + 14 + pkt.dict['ip']['header_len'] + pkt.dict[type]['header_len'] 
+            return (global_index,index,d['data'][index:])
                 
     return ()
 
